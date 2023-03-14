fbbe21c5607640458c7902b0f92387c44550ad06
Kushwanth P <Kushwanth.P@Cognizant.com>
2023-03-15 01:52:51 +0530

    Added redis integration

--- api/routes/staffs.js
+++ api/routes/staffs.js
@@ -3,12 +3,15 @@ const express = require('express')
 const router = express.Router()
 
 const StaffController = require('../controllers/staffController')
+const checkAuth = require('../middleware/checkAuth')
 
 
 router.post('/addStaff', StaffController.addStaff)
 
 router.post('/login', StaffController.staffLogin)
 
-router.delete('/:userId', StaffController.deleteStaff)
+router.post('/getStaffDetails', StaffController.getStaffDetails)
+
+router.delete('/:userId', checkAuth, StaffController.deleteStaff)
 
 module.exports = router
